1408927930da4ae7059b90bbad0b9fd58b10e556
vks16 <vishalsharma16171718@gmail.com>
2022-05-11 13:17:42 +0530

    chuck norris joke generator

--- app.js
+++ app.js
@@ -1,79 +1,22 @@
-document.getElementById("button1").addEventListener("click", loadCustomer);
-document.getElementById("button2").addEventListener("click", loadCustomers);
+document.querySelector(".get-jokes").addEventListener("click", getJokes);
 
+function getJokes(event){
+    event.preventDefault();
+    const number = document.getElementById("number").value;
 
-function loadCustomer(e) {
     const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "customer.json", true);
+    xhr.open("GET", `http://api.icndb.com/jokes/random/${number || 1}`, true);
+    
+    xhr.onload = function(){
+        if(this.status === 200){
+            const jokes = JSON.parse(this.responseText);
+            
+            const output = jokes.value.reduce((total,e) => total + `<li>${e.joke}</li>`, '')
 
-    xhr.onload = function () {
-        if (this.status === 200) {
-            const customer = JSON.parse(this.responseText);
-            document.getElementById("customer").innerHTML = `
-                <table class="u-full-width">
-                    <thead>
-                        <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>${customer.id}</td>
-                        <td>${customer.name}</td>
-                        <td>${customer.phone}</td>
-                        </tr>
-                    </tbody>    
-                </table>
-            `;
+            document.querySelector(".jokes").innerHTML = output;
         }
     }
-
-    xhr.onerror = function () {
-        alert("error")
-    }
-
     xhr.send();
-}
-
-
-function loadCustomers(e) {
-    const xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "customers.json", true);
-
-    xhr.onload = function () {
-        if (this.status === 200) {
-            const customers = JSON.parse(this.responseText);
-            document.getElementById("customer").innerHTML = `
-                <table class="u-full-width">
-                    <thead>
-                        <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                    ${customers.map(customer => `<tr>
-<td>${customer.id}</td>
-<td>${customer.name}</td>
-<td>${customer.phone}</td>
-</tr>`)
-                }
-                    
-                    </tbody>    
-                </table>
-            `;
-        }
-    }
-
-    xhr.onerror = function () {
-        alert("error")
-    }
-
-    xhr.send();
+    
 }
