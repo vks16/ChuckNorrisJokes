a6cc61f581a89c400b811ac436834ecf2898e51a
vks16 <vishalsharma16171718@gmail.com>
2022-05-11 12:33:49 +0530

    xhr

---
+++ app.js
@@ -0,0 +1,39 @@
+document.getElementById("button").addEventListener('click', loadData);
+
+function loadData() {
+    // Create an XHR Object
+    const xhr = new XMLHttpRequest();
+
+    // Open
+    xhr.open("GET", 'data.txt', true);
+
+    // Optional - Used for spinners / loaders
+    xhr.onprogress = function() {
+        // readystate 3
+
+    }
+
+    xhr.onload = function() {
+        if(this.status === 200){
+            console.log(this.responseText)
+        }
+    }
+
+    xhr.onerror = function() {
+        console.log('Request error...')
+    }
+    
+    xhr.send();
+
+    // readyState Values
+    // 0: request not initialized
+    // 1: server connection established
+    // 2: request received
+    // 3: processing request
+    // 4: request finished and response is ready
+
+    // HTTP Status
+    // 200: "OK"
+    // 403: "Forbidden"
+    // 404: "Not Found"
+}
